2072f0a31587c440dbbe2cb1be171109f4efbde3
venkatesh <external.Dumpa.VenkateswaraReddy@in.bosch.com>
2022-07-22 13:11:15 +0530

    point of service changes

--- custom/ceea/ceeafacades/src/com/ceea/facades/populators/CeeaProductPopulator.java
+++ custom/ceea/ceeafacades/src/com/ceea/facades/populators/CeeaProductPopulator.java
@@ -1,14 +1,20 @@
 package com.ceea.facades.populators;
 
 
+import com.ceea.facades.product.data.AnnotationData;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import de.hybris.platform.commercefacades.product.converters.populator.ProductPopulator;
 import de.hybris.platform.commercefacades.product.data.ImageData;
 import de.hybris.platform.commercefacades.product.data.ProductData;
 import de.hybris.platform.core.model.media.MediaModel;
 import de.hybris.platform.core.model.product.ProductModel;
 import de.hybris.platform.servicelayer.dto.converter.Converter;
+import org.apache.commons.lang.StringUtils;
 
 import javax.annotation.Resource;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
@@ -51,7 +57,29 @@ public class CeeaProductPopulator extends ProductPopulator
 		if(null!=primaryImg) {
 			target.setPrimaryImgURL(primaryImg.getURL());
 		}
+
+		target.setProductVideoUrl(source.getProductVideoUrl());
+		final MediaModel productPDF = source.getProductPDF();
+		if (productPDF != null)
+		{
+			final ImageData convert = getImageConverter().convert(productPDF);
+			target.setProductPDF(convert);
+		}
 		// super.populate(source, target);
+		try {
+			convertJsonToObject(target);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	private void convertJsonToObject(ProductData target) throws IOException {
+		ObjectMapper mapper = new ObjectMapper();
+		AnnotationData[] readValue=null;
+		if(StringUtils.isNotEmpty(target.getAnnotation())){
+			readValue = mapper.readValue(target.getAnnotation(), AnnotationData[].class);
+			List<AnnotationData> list = Arrays.asList(readValue);
+			target.setAnnotationList(list);
+		}
+
 	}
 }
-
