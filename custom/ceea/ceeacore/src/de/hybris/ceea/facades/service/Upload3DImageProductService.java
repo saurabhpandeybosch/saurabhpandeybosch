4f569351d624dbcbca576b5314c7773eb5066827
SrichandanKumarParida <106591218+SrichandanKumarParida@users.noreply.github.com>
2022-12-13 07:33:02 +0530

    Product_Api_Creation
    Features to create new Product Api

--- custom/ceea/ceeacore/src/de/hybris/ceea/facades/service/Upload3DImageProductService.java
+++ custom/ceea/ceeacore/src/de/hybris/ceea/facades/service/Upload3DImageProductService.java
@@ -1,7 +1,12 @@
 
 package de.hybris.ceea.facades.service;
 
+import de.hybris.platform.catalog.model.CatalogVersionModel;
+import de.hybris.platform.core.model.product.ProductModel;
+
 import org.springframework.web.multipart.MultipartFile;
+
+import com.ceea.facades.product.data.AnnotationData;
 
 
 /**
@@ -12,4 +17,11 @@ public interface Upload3DImageProductService
 {
 
 	String setProduct3DImageByCode(String code, MultipartFile file, String annotation);
+
+	String setProduct3DImageAndAnnotationByCode(String code, MultipartFile file, AnnotationData annotationData);
+
+	String setProduct2DImageByCode(String code, MultipartFile file);
+
+	ProductModel getProductForCode(final CatalogVersionModel catalogVersion, final String code);
+
 }
--- custom/ceea/ceeacore/src/de/hybris/ceea/facades/service/impl/Upload3DImageProductServiceImpl.java
+++ custom/ceea/ceeacore/src/de/hybris/ceea/facades/service/impl/Upload3DImageProductServiceImpl.java
@@ -3,8 +3,13 @@
  */
 package de.hybris.ceea.facades.service.impl;
 
+import static de.hybris.platform.servicelayer.util.ServicesUtil.validateIfSingleResult;
+import static java.lang.String.format;
+
 import de.hybris.ceea.core.event.Ceea3DImageSubmitEvent;
 import de.hybris.ceea.facades.service.Upload3DImageProductService;
+import de.hybris.platform.catalog.CatalogVersionService;
+import de.hybris.platform.catalog.model.CatalogVersionModel;
 import de.hybris.platform.core.model.media.MediaModel;
 import de.hybris.platform.core.model.product.ProductModel;
 import de.hybris.platform.product.ProductService;
@@ -13,10 +18,19 @@ import de.hybris.platform.servicelayer.media.MediaService;
 import de.hybris.platform.servicelayer.model.ModelService;
 
 import java.io.InputStream;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.ceea.core.product.dao.CustomProductDao;
+import com.ceea.facades.product.data.AnnotationData;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+
 
 
 /**
@@ -32,10 +46,46 @@ public class Upload3DImageProductServiceImpl implements Upload3DImageProductServ
 
 
 
-	ModelService modelService;
-	MediaService mediaService;
-	ProductService productService;
+	private ModelService modelService;
+	private MediaService mediaService;
+	private ProductService productService;
 	private EventService eventService;
+	private CatalogVersionService catalogVersionService;
+	private CustomProductDao customProductDao;
+
+	/**
+	 * @return the customProductDao
+	 */
+	public CustomProductDao getCustomProductDao()
+	{
+		return customProductDao;
+	}
+
+	/**
+	 * @param customProductDao
+	 *           the customProductDao to set
+	 */
+	public void setCustomProductDao(final CustomProductDao customProductDao)
+	{
+		this.customProductDao = customProductDao;
+	}
+
+	/**
+	 * @return the catalogVersionService
+	 */
+	public CatalogVersionService getCatalogVersionService()
+	{
+		return catalogVersionService;
+	}
+
+	/**
+	 * @param catalogVersionService
+	 *           the catalogVersionService to set
+	 */
+	public void setCatalogVersionService(final CatalogVersionService catalogVersionService)
+	{
+		this.catalogVersionService = catalogVersionService;
+	}
 
 	/**
 	 * @return the eventService
@@ -127,7 +177,6 @@ public class Upload3DImageProductServiceImpl implements Upload3DImageProductServ
 	}
 
 
-
 	@Override
 	public String setProduct3DImageByCode(final String code, final MultipartFile file, final String annotation)
 	{
@@ -153,7 +202,61 @@ public class Upload3DImageProductServiceImpl implements Upload3DImageProductServ
 			productmodel.setThreeDimensionalImage(mediaModel);
 			modelService.save(productmodel);
 
+			final Ceea3DImageSubmitEvent event = new Ceea3DImageSubmitEvent(productmodel);
+			this.eventService.publishEvent(event);
 
+
+			return "File saved Successfully";
+
+		}
+		catch (final Exception e)
+		{
+
+			e.printStackTrace();
+
+		}
+		return "Unable to save file";
+	}
+
+	@Override
+	public String setProduct3DImageAndAnnotationByCode(final String code, final MultipartFile file,
+			final AnnotationData annotation)
+	{
+
+		try (final InputStream inputStream = file.getInputStream())
+		{
+			final MediaModel mediaModel = modelService.create(MediaModel.class);
+
+			mediaModel.setCode(code + "-" + System.currentTimeMillis());
+			mediaModel.setRealFileName(file.getOriginalFilename());
+			mediaModel.setMime(file.getContentType());
+
+			modelService.save(mediaModel);
+			mediaService.setStreamForMedia(mediaModel, inputStream);
+			modelService.save(mediaModel);
+			modelService.refresh(mediaModel);
+
+			final ProductModel productmodel = productService.getProductForCode(code);
+
+			LOGGER.info("PRODUCTMODEL CODE:" + productmodel.getCode());
+			LOGGER.info("PRODUCTMODEL:" + productmodel.getCode());
+			final ObjectMapper mapper = new ObjectMapper();
+			final AnnotationData annotationData = new AnnotationData();
+			annotationData.setHotspotId(annotation.getHotspotId());
+			annotationData.setTransform(annotation.getTransform());
+			annotationData.setRotation(annotation.getRotation());
+			annotationData.setScale(annotation.getScale());
+			annotationData.setText(annotation.getText());
+			try
+			{
+				productmodel.setAnnotation(mapper.writeValueAsString(annotationData));
+			}
+			catch (final JsonProcessingException e)
+			{
+				e.printStackTrace();
+			}
+			productmodel.setThreeDimensionalImage(mediaModel);
+			modelService.save(productmodel);
 
 			final Ceea3DImageSubmitEvent event = new Ceea3DImageSubmitEvent(productmodel);
 			this.eventService.publishEvent(event);
@@ -168,16 +271,57 @@ public class Upload3DImageProductServiceImpl implements Upload3DImageProductServ
 			e.printStackTrace();
 
 		}
-
-
-
 		return "Unable to save file";
 	}
 
 
+	@SuppressWarnings("removal")
+	@Override
+	public String setProduct2DImageByCode(final String code, final MultipartFile file)
+	{
+
+		//final CatalogVersionModel catalogVersion = catalogVersionService.getCatalogVersion("apparelProductCatalog", "STAGED");
+		try
+		{
+			final MediaModel mediaModel = modelService.create(MediaModel.class);
+			mediaModel.setCode(code + "-" + System.currentTimeMillis());
+			mediaModel.setRealfilename(file.getOriginalFilename());
+			mediaModel.setMime(file.getContentType());
+			//mediaModel.setCatalogVersion(catalogVersion);
+
+			modelService.save(mediaModel);
+			mediaService.setStreamForMedia(mediaModel, file.getInputStream());
+			modelService.refresh(mediaModel);
+
+			final ProductModel productModel = productService.getProductForCode(code);
+
+			productModel.setPicture(mediaModel);
+			modelService.save(productModel);
+
+			return "Image Saved Successfully";
+
+		}
+		catch (final Exception e)
+		{
+			e.printStackTrace();
+		}
+		return "Unable to upload image";
+	}
 
 
 
+	public ProductModel getProductForCode(final CatalogVersionModel catalogVersion, final String code)
+	{
+		final List<ProductModel> products = getCustomProductDao().findProductsByCode(catalogVersion, code);
+
+		validateIfSingleResult(products,
+				format("Product with code '%s' and CatalogVersion '%s.%s' not found!", code, catalogVersion.getCatalog().getId(),
+						catalogVersion.getVersion()),
+				format("Code '%s' and CatalogVersion '%s.%s' are not unique. %d products found!", code,
+						catalogVersion.getCatalog().getId(), catalogVersion.getVersion(), Integer.valueOf(products.size())));
+
+		return products.get(0);
+	}
 
 
 }
